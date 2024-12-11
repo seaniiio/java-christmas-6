1222a2a4fa984fcaa72e21992399f3c3e6e15535
seaniiio <cnc4750@naver.com>
2024-12-11 19:18:30 +0900

    feat: 주어진 메뉴 저장

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+public enum Menu {
+    YANG_SONG_SOUP(MenuCategory.APPETIZER, "양송이수프", 6_000),
+    TAPAS(MenuCategory.APPETIZER, "타파스", 5_500),
+    CAESAR_SALAD(MenuCategory.APPETIZER, "시저샐러드", 8_000),
+
+    TBONE_STEAK(MenuCategory.MAIN, "티본스테이크", 55_000),
+    BARBECUE_RIBS(MenuCategory.MAIN, "바베큐립", 54_000),
+    SEAFOOD_PASTA(MenuCategory.MAIN, "해산물파스타", 35_000),
+    CHRISTMAS_PASTA(MenuCategory.MAIN, "크리스마스파스타", 25_000),
+
+    CHOCOLATE_CAKE(MenuCategory.DESSERT, "초코케이크", 15_000),
+    ICE_CREAM(MenuCategory.DESSERT, "아이스크림", 5_000),
+
+    ZERO_COKE(MenuCategory.BEVERAGE, "제로콜라", 3_000),
+    RED_WINE(MenuCategory.BEVERAGE, "레드와인", 60_000),
+    CHAMPAGNE(MenuCategory.BEVERAGE, "샴페인", 25_000),
+    ;
+
+    private final MenuCategory menuCategory;
+    private final String name;
+    private final int price;
+
+    Menu(MenuCategory menuCategory, String name, int price) {
+        this.menuCategory = menuCategory;
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/main/java/christmas/domain/MenuCategory.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+public enum MenuCategory {
+
+    APPETIZER("에피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    BEVERAGE("음료"),
+    ;
+
+
+    private final String name;
+
+    MenuCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
